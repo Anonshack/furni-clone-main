db292e4e3d3484666c5a9b93bf697cdf536ab9bb
Anonshack <anonshack48@gmail.com>
2024-10-09 12:50:48 +0500

    test

--- config/urls.py
+++ config/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('', include('homepage.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
