16f39775a6e50ad24e1acea81d7db599f6d82608
Кудратбек Казакжонов <140953705+Anonshack@users.noreply.github.com>
2025-11-15 12:10:26 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'qaxramonovashaxzoda2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'fqhu titm affv vzkp'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'email password'
